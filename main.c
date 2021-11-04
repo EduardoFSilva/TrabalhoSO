4ce7017a3e1f061c704ef1333763b1e86352c586
Alberto <jrjotinha08@gmail.com>
2021-11-04 09:06:40 -0300

    Correção de bugs que ocorriam ao digitar o comando sair

--- main.c
+++ main.c
@@ -11,64 +11,59 @@
 
 #define MAX_LINE 100 //tamanho maximo da linha de comando
 
-int main()
-{
+int main() {
 
     char comando[MAX_LINE]; //string que recebera o comando
     char **args;            //ponteiro de argumentos
     int argc = 0;           //contador de argumentos
 
-    while (strcmp(comando, "sair") != 0)
-    {
+    while (1) {
+
         printf("$ ");
         fgets(comando, MAX_LINE, stdin);     //recebendo string via teclado
         comando[strlen(comando) - 1] = '\0'; // removendo \n e inserindo o caractere nulo
+
+        if (strcmp(comando, "sair") == 0) exit(0); //se o comando digitado for "sair"
 
         //tratar os erros
         //criar um processo para cada novo comando
         int pid = fork();
 
-        if (pid == 0 && strcmp(comando, "sair") != 0)
-        {
+        if (pid == 0 ){ //está no processo filho
             int i = 0;
 
-            for (i = 0; i < strlen(comando); i++)
-            {
+            for (i = 0; i < strlen(comando); i++) { //conta a quantidade de argumentos
 
                 if (comando[i] == ' ')
                     argc++;
             }
 
             //aloca vetor de strings baseado na quantidade de argumentos
-            args = (char **)malloc((argc + 1) * sizeof(char *));
+            args = (char **) malloc((argc + 1) * sizeof(char *));
 
             //o token será o comando de entrada
             char *token = strtok(comando, " ");
-      
+
             //inserindo o token na primeira posicao
             args[0] = token;
-                      
+
             i = 1;
 
             //preenchendo o restante do array de strings com os argumentos
-            while (token != NULL)
-            {
-
+            while (token != NULL) {
                 args[i] = strtok(NULL, " ");
                 token = strtok(NULL, " ");
                 i++;
             }
-            
+
             //envia argumentos pro exec
             execvp(args[0], args);
-        }
-        else if (pid > 0)
-        {
+        } else if (pid > 0) {
             //espera o processo filho terminar
             //printf("\nProcesso filho finalizou");
             wait(NULL);
         }
     }
 
-    exit(0);
+
 }
