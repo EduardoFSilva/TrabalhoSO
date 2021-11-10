86a43151549c86e207cb6dcea0a38dd95018bb12
Alberto <0056119@academico.ifmg.edu.br>
2021-11-10 08:39:04 -0300

    Separação da Função que Monta o Comando + Correçao do Bug "sair"

--- main.c
+++ main.c
@@ -3,7 +3,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
+#include <assert.h>
 //Bibliotecas do UNIX
 #include <unistd.h>
 #include <sys/types.h>
@@ -14,20 +14,20 @@
 
 #define PATH_MAX 1024//tamanho maximo da string do path
 #define MAX_LINE 100 //tamanho maximo da linha de comando
+#define READ_END 0 //Ponta de leitura
+#define WRITE_END 1 //Ponta de escrita
+
+
+char **linha_Comando(char *comando);
 
 int main() {
 
-    char comando[MAX_LINE]; //string que recebera o comando
-    char **args;            //ponteiro de argumentos
-    int argc = 0;           //contador de argumentos
-    char cwd[PATH_MAX];     //variavel que contem o path do arquivo
-
-    
+    char comando[MAX_LINE], cwd[PATH_MAX]; //string que recebera o comando e Path
 
     while (1) {
 
         getcwd(cwd, sizeof(cwd));            //comando getcwd pega o path do arquivo
-        printf("%s$ ",cwd);
+        printf("%s$ ", cwd);
         fgets(comando, MAX_LINE, stdin);     //recebendo string via teclado
         comando[strlen(comando) - 1] = '\0'; // removendo \n e inserindo o caractere nulo
 
@@ -36,42 +36,62 @@ int main() {
         //tratar os erros
         //criar um processo para cada novo comando
         int pid = fork();
+        assert(pid > 0 || pid == 0); //vai assegurar de que o processo tenha sido alocado com sucesso
 
-        if (pid == 0 ){ //está no processo filho
+        if (pid == 0) { //está no processo filho
             int i = 0;
 
-            for (i = 0; i < strlen(comando); i++) { //conta a quantidade de argumentos
+            char **args = linha_Comando(comando);
 
-                if (comando[i] == ' ')
-                    argc++;
+            //enviando argumentos pro exec
+            int error = execvp(args[0], args);
+
+            if (error == -1) { //funcao execvp() retorna -1 em caso de erro
+
+                fprintf(stderr, "Comando desconhecido\n");
+                exit(1);
             }
 
-            //aloca vetor de strings baseado na quantidade de argumentos
-            args = (char **) malloc((argc + 1) * sizeof(char *));
-
-            //o token será o comando de entrada
-            char *token = strtok(comando, " ");
-
-            //inserindo o token na primeira posicao
-            args[0] = token;
-
-            i = 1;
-
-            //preenchendo o restante do array de strings com os argumentos
-            while (token != NULL) {
-                args[i] = strtok(NULL, " ");
-                token = strtok(NULL, " ");
-                i++;
-            }
-
-            //envia argumentos pro exec
-            execvp(args[0], args);
         } else if (pid > 0) {
+
             //espera o processo filho terminar
-            //printf("\nProcesso filho finalizou");
             wait(NULL);
+        } else {
+
+            fprintf(stderr, "Erro ao alocar processo !");
+            exit(1);
+
         }
     }
 
+
+}
+
+char **linha_Comando(char *comando) {
+
+    char **args = NULL;
+    int argc = 0, i = 0;
+
+    for (i = 0; i < strlen(comando); i++) { //conta a quantidade de argumentos
+
+        if (comando[i] == ' ')
+            argc++;
+    }
+
+    //aloca vetor de strings baseado na quantidade de argumentos
+    args = (char **) malloc((argc + 1) * sizeof(char *));
+
+    //inserindo o token na primeira posicao
+    i = 0;
+    char *token = strtok(comando, " ");
+    //preenchendo o restante do array de strings com os argumentos
+    while (token != NULL) {
+        args[i] = token;
+        token = strtok(NULL, " ");
+        i++;
+    }
+     args[i] = NULL;
+
+    return args;
 
 }
